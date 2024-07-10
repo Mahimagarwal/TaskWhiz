5135f6e3ed8368acff35aa2a9a4688e1098e326e
giridhar <giridharpp07@gmail.com>
2024-07-11 00:55:25 +0530

    Added Search Functionality

--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -1,5 +1,6 @@
 import Templates from "@/app/(data)/Templates"
 import TemplateCard from "./TemplateCard"
+import { useEffect, useState } from "react"
 export interface TEMPLATE{
     name:string,
     desc:string,
@@ -15,10 +16,25 @@ export interface FORM{
     name:string,
     required?:boolean
 }
-const TemplateListSection = () => {
+const TemplateListSection = ({userSearchInput}:any) => {
+    const [templateList,setTemplateList]=useState(Templates)
+    useEffect(()=>{
+   if(userSearchInput)
+   {
+     const filterdata=Templates.filter(item=>
+        item.name.toLowerCase().includes(userSearchInput.toLowerCase())
+     );
+     setTemplateList(filterdata);
+   }
+   else
+   {
+    setTemplateList(Templates)
+   }
+    },[userSearchInput])
+    
     return (
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10'>
-            {Templates.map((item:TEMPLATE,index:number)=>(
+            {templateList.map((item:TEMPLATE,index:number)=>(
              <TemplateCard {...item}/>
             ))}
         </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,15 @@
+"use client"
+import { useState } from "react"
 import SearchSection from "./_components/SearchSection"
 import TemplateListSection from "./_components/TemplateListSection"
 
 
 const page = () => {
+  const [userSearchInput,setUserSearchInput]=useState<string>()
   return (
     <div>
-        <SearchSection/>
-        <TemplateListSection/>
+        <SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)}/>
+        <TemplateListSection userSearchInput={userSearchInput}/>
     </div>
   )
 }
