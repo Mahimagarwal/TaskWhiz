618dec95a344f7b6a5befcae008a08fd6aec3704
giridhar <giridharpp07@gmail.com>
2024-07-11 02:15:03 +0530

    added markdown page

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import FormSection from "../_components/FormSection"
-import OutputSection from "../_components/OutputSection"
+import FormSection from "../-components/FormSection"
+import OutputSection from "../-components/OutputSection"
 import Templates from "@/app/(data)/Templates"
 import { TEMPLATE } from "../../_components/TemplateListSection"
 import Link from "next/link"
@@ -15,6 +15,9 @@ interface PROPS{
 
 const page = (props:PROPS) => {
     const selectedTemplate:TEMPLATE|undefined=Templates?.find((item)=>item.slug)
+    const GenerateAIContent=(formData:any)=>{
+       
+    }
     return (
         <div className='p-5'>
             <Link href={"/dashboard"}>
@@ -24,12 +27,12 @@ const page = (props:PROPS) => {
                 {/* FormSection  */}
                     <FormSection 
                     selectedTemplate={selectedTemplate}
-                    userFormInput={(v:any)=>console.log(v)}
+                    userFormInput={(v:any)=>GenerateAIContent(v)}
                     // loading={loading} 
                     />
                 {/* OutputSection  */}
                 <div className='col-span-2'>
-                    {/* <OutputSection aiOutput={aiOutput} /> */}
+                    <OutputSection  />
                     </div>
             </div>
         </div>
--- app/dashboard/content/_components/OutputSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OutputSection = () => {
-  return (
-    <div>OutputSection</div>
-  )
-}
-
-export default OutputSection
